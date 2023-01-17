2879a164b44192d571a46e62a164f3466e3894d3
Somye Mahajan <anyone.mahajan@gmail.com>
2023-01-18 02:21:11 +0530

    Updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,34 +29,42 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: ChromeCastButton(
-            color: Colors.red,
-            onButtonCreated: (controller) {
-              castController = controller;
-              castController?.addSessionListener();
-            },
-            onSessionEnding: (position) {
-              print("in app ending");
-              print("position is $position");
-            },
-            onSessionEnded: () {
-              print("in app ended");
-            },
-            onSessionStarted: () {
-              print("in app started");
-              castController?.loadMedia(
-                url:
-                    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-                position: 30000,
-                autoplay: true,
-                title: "Spider-Man: No Way Home",
-                description:
-                    "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
-                image:
-                    "https://terrigen-cdn-dev.marvel.com/content/prod/1x/marvsmposterbk_intdesign.jpg",
-                type: ChromeCastMediaType.movie,
-              );
-            },
+          child: Column(
+            children: [
+              const Text("ChromeCastButton"),
+              const SizedBox(
+                height: 5,
+              ),
+              ChromeCastButton(
+                color: Colors.red,
+                onButtonCreated: (controller) {
+                  castController = controller;
+                  castController?.addSessionListener();
+                },
+                onSessionEnding: (position) {
+                  print("in app ending");
+                  print("position is $position");
+                },
+                onSessionEnded: () {
+                  print("in app ended");
+                },
+                onSessionStarted: () {
+                  print("in app started");
+                  castController?.loadMedia(
+                    url:
+                        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                    position: 30000,
+                    autoplay: true,
+                    title: "Spider-Man: No Way Home",
+                    description:
+                        "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+                    image:
+                        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/marvsmposterbk_intdesign.jpg",
+                    type: ChromeCastMediaType.movie,
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
